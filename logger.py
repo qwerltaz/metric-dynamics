50cfac67d87b82790cbb6190678be613395f5656
Marcin Sroka <msmarcinpl@gmail.com>
2024-04-07 16:58:12 +0200

    add logger, tweak parse and schedule

---
+++ logger.py
@@ -0,0 +1,44 @@
+import logging
+import logging.config
+
+
+def get_logger() -> logging.Logger:
+    logging_config = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "simple": {
+                "format": "[%(levelname)s] %(message)s"
+            },
+            "verbose": {
+                "format": "[%(levelname)s] %(asctime)s: %(message)s",
+                "datefmt": "%Y-%m-%dT%H:%M:%S%z"
+            }
+        },
+        "handlers": {
+            "stdout": {
+                "class": "logging.StreamHandler",
+                "level": "INFO",
+                "formatter": "simple",
+                "stream": "ext://sys.stdout"
+            },
+            "file": {
+                "class": "logging.FileHandler",
+                "level": "DEBUG",
+                "formatter": "verbose",
+                "filename": ".log",
+                "mode": "w"
+            }
+        },
+        "loggers": {
+            "local_logger": {
+                "level": "DEBUG",
+                "handlers": ["stdout", "file"]
+            }
+        }
+    }
+
+    logger = logging.getLogger("local_logger")
+    logging.config.dictConfig(logging_config)
+
+    return logger
--- schedule.py
+++ schedule.py
@@ -7,20 +7,24 @@ import pandas as pd
 from parse import MetricParse
 
 
-def schedule_repositories(repo_urls_csv_path: str) -> None:
+def schedule_repositories(repo_urls_csv_path: str, custom_save_path: str = None) -> None:
     """
     Schedule metrics collection for a number repositories, from a csv file with repository names and urls.
 
     :param repo_urls_csv_path: path to csv file with columns `name` and `repo_url`
+    :param custom_save_path: custom path to save metrics. Default is `data/results`
     """
     repo_urls = pd.read_csv(repo_urls_csv_path)
 
-    # TODO: try saving progress in a file, to continue from where it stopped
+    # TODO: try saving progress to continue from where it stopped
 
     for i, row in repo_urls.iterrows():
         repo_name = row["name"]
         repo_url = row["repo_url"]
         metric_parse = MetricParse(repo_url)
+
+        metric_save_path = custom_save_path or os.path.join("data", "results", repo_name)
         metric_parse.save_metrics_for_each_commit()  # TODO manually set results path
+
 
 os.path.join("data", "results", "tiny")
