b22f1a88fbd33587c64b3d24968d223c159d472d
Marcin Sroka <msmarcinpl@gmail.com>
2024-04-15 17:35:46 +0200

    tweaks, add 'autosave' for scheduler

--- schedule.py
+++ schedule.py
@@ -1,33 +1,41 @@
-"""Schedule metrics collection for a number repositories."""
-
-import os
+"""Schedule metrics collection for a number of repositories."""
 
 import pandas as pd
 
 from parse import MetricParse
 
 
-def schedule_repositories(repo_urls_csv_path: str, custom_save_path: str = None) -> None:
+def schedule_repositories(repo_urls_csv_path: str) -> None:
     """
-    Schedule metrics collection for a number repositories, from a csv file with repository names and urls.
+    Schedule metrics collection for a number of repositories, from a csv file with repository names and urls.
 
     :param repo_urls_csv_path: path to csv file with columns `name` and `repo_url`
-    :param custom_save_path: custom path to save metrics. Default is `data/results`
     """
     repo_urls = pd.read_csv(repo_urls_csv_path, encoding="utf-8")
 
-    # TODO: try saving progress to continue from where it stopped
+    # Column to track which repositories have been computed.
+    if "computed" not in repo_urls.columns:
+        repo_urls["computed"] = False
 
     for i, row in repo_urls.iterrows():
-        repo_name = row["name"]
+        if row["computed"]:
+            continue
+
         repo_url = row["repo_url"]
         metric_parse = MetricParse(repo_url)
 
         metric_parse.save_metrics_for_each_commit()
+        repo_urls.loc[i, "computed"] = True
+
+        repo_urls.to_csv(repo_urls_csv_path, index=False, encoding="utf-8")
 
 
 def main():
+    # Tiny test table.
     schedule_repositories("data/url/urls_tiny.csv")
+
+    # Full table.
+    # schedule_repositories("data/url/pypi_top_1000.csv")
 
 
 if __name__ == "__main__":
