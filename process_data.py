3c700a8ca9c85993d6d8010af8e26d1b3617ebe9
qwerltaz <msmarcinpl@gmail.com>
2024-05-06 13:19:39 +0200

    add multiple mode support to data processor

--- process_data.py
+++ process_data.py
@@ -2,49 +2,67 @@
 
 import argparse
 import os
+from typing import Literal
 
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
 from parse import DATA_DIR
 
 
-def merge_results():
+def process_results(mode: Literal["merge"] = "merge"):
     """Merge all results into a separate table and save to the results directory."""
     merge_dir = os.path.join(DATA_DIR, "results")
-
-    all_results_dataframes_list = []
     all_results = pd.DataFrame()
 
-    for file in os.listdir(merge_dir):
+    # Get all dataframes from the directory.
+    all_results_dataframes_list = get_results_df_list(merge_dir)
+
+    if mode == "merge":
+        all_results = pd.concat(all_results_dataframes_list, ignore_index=True)
+        save_path = "_all_results.csv"
+    else:
+        raise ValueError("Invalid mode.")
+
+    all_results = process_default(all_results)
+    all_results.to_csv(os.path.join(DATA_DIR, "results", save_path), encoding="utf-8")
+
+
+def get_results_df_list(path: str):
+    """Get a list of dataframes from the directory."""
+    df_list = []
+    for file in os.listdir(path):
         if file.endswith(".csv"):
-            results = pd.read_csv(os.path.join(merge_dir, file), encoding="utf-8")
+            results = pd.read_csv(os.path.join(path, file), encoding="utf-8")
 
             # Add repo name column to the results.
             repo_name = file.split(".")[0]
             results["repo_name"] = repo_name
 
-            all_results_dataframes_list.append(results)
+            df_list.append(results)
 
-    # Concatenate, reset index, drop duplicates.
-    all_results = pd.concat(all_results_dataframes_list, ignore_index=True)
-    all_results.drop(columns=["ID"], inplace=True)
-    all_results.drop_duplicates(subset="hash", inplace=True)
-    all_results.reset_index(drop=True, inplace=True)
-    all_results.index.name = "ID"
+    return df_list
 
-    # all_results.drop(columns=["commit_message"], inplace=True)
-    all_results["commit_message"] = all_results["commit_message"].str.replace("\r\n", " ")
 
-    all_results.to_csv(os.path.join(DATA_DIR, "results", "_all_results.csv"), encoding="utf-8")
+def process_default(df: pd.DataFrame):
+    """Default data processing."""
+    df.drop(columns=["ID"], inplace=True)
+    df.drop_duplicates(subset="hash", inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    df.index.name = "ID"
+
+    df["commit_message"] = df["commit_message"].str.replace("\r\n", " ")
+
+    return df
 
 
 def main():
     parser = argparse.ArgumentParser(description="Process data.")
-    parser.add_argument("--merge-results", action="store_true", help="Merge all results into a separate table.")
+    parser.add_argument("--mode", "-m", choices=["merge"], help="Mode of processing the data.",)
     args = parser.parse_args()
 
-    if args.merge_results:
-        merge_results()
+    if args.mode == "merge":
+        process_results()
         print("Merged results.")
 
 
